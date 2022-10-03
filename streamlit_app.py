6646151918da923d1f216410960582f5be8b961c
Mike Salem <mike.f.salem@gmail.com>
2022-10-02 20:58:06 -0700

    pep8 req change

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ Idea for app: https://medium.com/analytics-vidhya/text-summarization-in-python-u
 """
 
 # -- Standard Imports
+from importlib import import_module
 import os
+import subprocess
 import time
 
 # -- Third-party Imports
@@ -23,8 +25,16 @@ import streamlit as st
 __author__ = "msalem0056"
 __created__ = "October 2022"
 
+
 @st.cache()
-def get_news(search_text : str) -> pd.DataFrame:
+def download_and_init_nlp(model_name: str):
+    """Downloads the pretrained language model"""
+    subprocess.run(['python', '-m', 'spacy', 'download', f'{model_name}'])
+    return spacy.load(model_name)
+
+
+@st.cache()
+def get_news(search_text: str) -> pd.DataFrame:
     """
     Gets the list of articles from a Google Search
 
@@ -34,14 +44,15 @@ def get_news(search_text : str) -> pd.DataFrame:
         Returns:
                 dataframe (pd.DataFrame): Information collected from the search
     """
+    if search_text == "":
+        return None
     gn = GoogleNews()
     # Note the when can be changed if desired
-    top = gn.search(search_text, when = '1d')
-    entries = top["entries"]
-    return pd.DataFrame(entries)
+    gn.search(search_text)
+    return gn.results(sort=True)
 
 
-def read_article(text : str) -> list[str]:        
+def read_article(text: str) -> list[str]:
     """
     Reads and article and cleans up sentences
 
@@ -50,14 +61,15 @@ def read_article(text : str) -> list[str]:
 
         Returns:
                 list (list[str]): sentences in list format
-    """    
-    sentences =[]        
-    sentences = tokenizer(text)    
-    for sentence in sentences:        
-        sentence = str(sentence).replace("[^a-zA-Z0-9]"," ")     
+    """
+    sentences = []
+    sentences = tokenizer(text)
+    for sentence in sentences:
+        sentence = str(sentence).replace("[^a-zA-Z0-9]", " ")
     return sentences
 
-def sentence_similarity(s1 : str, s2 : str, nlp) -> float:
+
+def sentence_similarity(s1: str, s2: str, nlp) -> float:
     """
     Calculates the similarity between two sentences
 
@@ -68,12 +80,13 @@ def sentence_similarity(s1 : str, s2 : str, nlp) -> float:
 
         Returns:
                 float (float): sentence similarity score [0,1]
-    """   
+    """
     doc1 = nlp(s1)
     doc2 = nlp(s2)
     return doc1.similarity(doc2)
 
-def build_similarity_matrix(sentences : list[str], nlp) -> np.matrix:
+
+def build_similarity_matrix(sentences: list[str], nlp) -> np.matrix:
     """
     Build similarity matrix between sentences
 
@@ -83,23 +96,25 @@ def build_similarity_matrix(sentences : list[str], nlp) -> np.matrix:
 
         Returns:
                 matrix (np.matrix): matrix of similarity scores
-    """       
-    #create an empty similarity matrix
-    similarity_matrix = np.zeros((len(sentences),len(sentences)))        
+    """
+    # create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
     for idx1 in range(len(sentences)):
         for idx2 in range(len(sentences)):
-            if idx1!=idx2:
-                similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1].text,sentences[idx2].text, nlp)
+            if idx1 != idx2:
+                similarity_matrix[idx1][idx2] = sentence_similarity(
+                    sentences[idx1].text, sentences[idx2].text, nlp)
     return similarity_matrix
 
-def tl_dr(title, title_detail, links, link, id, guidislink, published, published_parsed, summary, summary_detail, source, sub_articles):
-    """Grabs the article description from the URL. Summarizes it and collects some stats.""" 
+
+def tl_dr(title, media, date, datetime, desc, link, img):
+    """Grabs the article description from the URL. Summarizes it and collects some stats."""
     try:
         article = newspaper.Article(url=link, language='en')
         article.download()
-        time.sleep(.01)        
+        time.sleep(.01)
         article.parse()
-        article ={
+        article = {
             "title": str(article.title),
             "text": str(article.text),
             'link': link,
@@ -110,17 +125,17 @@ def tl_dr(title, title_detail, links, link, id, guidislink, published, published
             "keywords": article.keywords,
             "summary": str(article.summary),
             "text_len": None,
-            "summary_len": None,          
-        }        
+            "summary_len": None,
+        }
 
         summary = ""
-        if len(article["text"]):            
+        if len(article["text"]):
             summarize_text = []
             num_sent = 5
 
             # Create a Tokenizer with the default settings for English
             # including punctuation rules and exceptions
-            tokenizer = nlp.tokenizer 
+            tokenizer = nlp.tokenizer
 
             # Build sentence similarity matrix
             sentences = list(nlp(article['text']).sents)
@@ -131,19 +146,20 @@ def tl_dr(title, title_detail, links, link, id, guidislink, published, published
             scores = nx.pagerank(sentence_sim_graph)
 
             # Sort by highest score and create summary
-            ranked_sentences = sorted(((scores[i],s.text) for i,s in enumerate(sentences)),reverse=True)
+            ranked_sentences = sorted(
+                ((scores[i], s.text) for i, s in enumerate(sentences)), reverse=True)
             for i in range(num_sent):
-                summarize_text.append(ranked_sentences[i][1])    
-            summary, _ = " ".join(summarize_text),len(sentences)
+                summarize_text.append(ranked_sentences[i][1])
+            summary, _ = " ".join(summarize_text), len(sentences)
 
             article['summary'] = summary
             article['text_len'] = len(str(article['text']))
             article['summary_len'] = len(summary)
-    except:
-        article ={
+    except BaseException:
+        article = {
             "title": None,
             "text": None,
-            'link': None,        
+            'link': None,
             "authors": None,
             "published_date": None,
             "top_image": None,
@@ -153,58 +169,66 @@ def tl_dr(title, title_detail, links, link, id, guidislink, published, published
             'summary': None,
             'text_len': None,
             'summary_len': None
-        }   
-        return article
+        }
+    return article
 
 
 # -- Main
 if __name__ == "__main__":
-    try:
-        nlp = spacy.load("en_core_web_sm")
-        print("here")
-    except:
-        from spacy.cli import download
-        download("en_core_web_sm")
-        nlp = spacy.load("en_core_web_sm")
-    
+
+    nlp = download_and_init_nlp("en_core_web_md")
     st.sidebar.write("# Controls and Stats")
     search_text = st.sidebar.text_area('Items to search here:')
-
-    # Get news and limit to 10 items for speed / processing
-    df = get_news(search_text)
-    df = df.head(10)
 
     st.markdown("# tl;dr News")
     st.markdown("## News for busy people")
     st.markdown("""Disclaimer: This is not intended to be a commercial product, solution, or tool. Any likeness to any commercial or personal products are purely coincidental.""")
 
-    if df.shape[0]:
+    # Get news and limit to 10 items for speed / processing
+    df = pd.DataFrame(get_news(search_text))
+
+    if df is not None and df.shape[0]:
+        df = df.head(10)
         with WorkerPool(n_jobs=os.cpu_count() - 1) as pool:
-            results = pool.map(tl_dr, list(df.itertuples(index=False, name=None)))
+            results = pool.map(
+                tl_dr, list(
+                    df.itertuples(
+                        index=False, name=None)))
         stats_news = []
+        print(results)
         for result in results:
+            if result['title'] is None:
+                continue
             try:
-                st.markdown('### <a href="{}" rel="noopener noreferrer" target="_blank">{}</a>'.format(result['link'], result['title']),unsafe_allow_html=True)
+                st.markdown(
+                    '### <a href="{}" rel="noopener noreferrer" target="_blank">{}</a>'.format(
+                        result['link'], result['title']), unsafe_allow_html=True)
                 col1, col2 = st.columns(2)
-                col1.markdown(result['link'].replace("https://","").replace("www.","").split("/")[0])
-                col2.markdown(result['published_date'] if result['published_date'] != "None" else "")
+                col1.markdown(
+                    result['link'].replace(
+                        "https://",
+                        "").replace(
+                        "www.",
+                        "").split("/")[0])
+                col2.markdown(
+                    result['published_date'] if result['published_date'] != "None" else "")
                 try:
                     st.image(result['top_image'])
-                except:
-                    pass            
+                except BaseException:
+                    pass
                 st.markdown("### Summary")
                 st.markdown(result['summary'])
-                st.markdown("***")           
-                stats_news.append(result) 
-            except:
+                st.markdown("***")
+                stats_news.append(result)
+            except BaseException:
                 pass
 
         stats_df = pd.DataFrame(stats_news)
-        d = stats_df.set_index("link")
+        stats_df = stats_df.set_index("link")
         fig = stats_df['text_len'].hist()
         st.sidebar.write("Document Character Length")
         st.sidebar.bar_chart(stats_df.text_len)
 
         fig = stats_df['summary_len'].hist()
         st.sidebar.write("Summary Character Length")
-        st.sidebar.bar_chart(stats_df.summary_len)    
+        st.sidebar.bar_chart(stats_df.summary_len)
